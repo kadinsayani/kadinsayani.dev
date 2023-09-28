0e2c2b4493646032b99f619d5b49e02d754ff8ee
Kadin Sayani <me@kadinsayani.dev>
2023-09-28 16:56:37 -0600

    removed pacman loader

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -3,37 +3,37 @@ import Me from "../components/me";
 import Apps from "../components/apps";
 import Projects from "../components/projects";
 import Header from "../components/header";
-import { useEffect, useState } from "react";
-import { PacmanLoader } from "react-spinners";
+import { useEffect } from "react";
+// import { PacmanLoader } from "react-spinners";
 
 function Root() {
-  const [isLoading, setIsLoading] = useState(true);
+  // const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setIsLoading(false);
-    }, 2000);
+  // useEffect(() => {
+  //   const timer = setTimeout(() => {
+  //     setIsLoading(false);
+  //   }, 1000);
 
-    return () => {
-      clearTimeout(timer);
-    };
-  }, []);
+  //   return () => {
+  //     clearTimeout(timer);
+  //   };
+  // }, []);
 
   return (
     <div>
-      {isLoading ? (
+      {/* {isLoading ? (
         <div className="center-container">
           <PacmanLoader color="#ffffffde" aria-label="Loading Spinner" />
         </div>
-      ) : (
-        <div>
-          <Header />
-          <Me />
-          <Apps />
-          <Projects />
-          <p>Kadin Sayani © 2023</p>
-        </div>
-      )}
+      ) : ( */}
+      <div>
+        <Header />
+        <Me />
+        <Apps />
+        <Projects />
+        <p>Kadin Sayani © 2023</p>
+      </div>
+      {/* )} */}
     </div>
   );
 }
