660cbf3a14914cbc352f13d5000d2979d05a262d
Kadin Sayani <me@kadinsayani.dev>
2023-09-28 17:08:57 -0600

    styling

--- src/routes/project.jsx
+++ src/routes/project.jsx
@@ -24,7 +24,7 @@ const divStyle = {
   alignItems: "center",
   justifyContent: "center",
   backgroundSize: "cover",
-  height: "800px",
+  height: "100vh",
 };
 
 const buttonStyle = {
@@ -59,63 +59,67 @@ function Project(props) {
       <button onClick={back} className="back-button">
         <IoArrowBack size={40} color={"#ffffffde"} />
       </button>
-      <h1>{project.name}</h1>
-      <div className="contact">
-        <a href={project.repo}>
-          <IoLogoGithub size="25px" />
-        </a>
-        <a href={project.link}>
-          <IoLink size="25px" />
-        </a>
+      <div className="project-item-container">
+        <h1>{project.name}</h1>
+        <div className="contact">
+          <a href={project.repo}>
+            <IoLogoGithub size="25px" />
+          </a>
+          <a href={project.link}>
+            <IoLink size="25px" />
+          </a>
+        </div>
+        <h2>{project.project}</h2>
+        <p>{project.description}</p>
+        <div className="skip">
+          <h2>Detailed Sketches</h2>
+          <div className="slide-container">
+            <Slide {...properties}>
+              {project.conceptImages.map((slideImage, index) => (
+                <div key={index}>
+                  <div
+                    style={{
+                      ...divStyle,
+                      backgroundImage: `url(${slideImage.url})`,
+                    }}
+                  >
+                    <span style={spanStyle}>{slideImage.caption}</span>
+                  </div>
+                </div>
+              ))}
+            </Slide>
+          </div>
+          <p>{project.conceptWriteup}</p>
+          <h2>Concept Sketches</h2>
+          <div className="slide-container">
+            <Slide {...properties}>
+              {project.detailedImages.map((slideImage, index) => (
+                <div key={index}>
+                  <div
+                    style={{
+                      ...divStyle,
+                      backgroundImage: `url(${slideImage.url})`,
+                    }}
+                  >
+                    <span style={spanStyle}>{slideImage.caption}</span>
+                  </div>
+                </div>
+              ))}
+            </Slide>
+          </div>
+          <p>{project.detailedWriteup}</p>
+          <h2>Video</h2>
+          <iframe
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/RtVM1qv3i3Y?si=p7L_hvGaUazbKtmp"
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowfullscreen
+          ></iframe>
+        </div>
       </div>
-      <h2>{project.project}</h2>
-      <p>{project.description}</p>
-      <h2>Detailed Sketches</h2>
-      <div className="slide-container">
-        <Slide {...properties}>
-          {project.conceptImages.map((slideImage, index) => (
-            <div key={index}>
-              <div
-                style={{
-                  ...divStyle,
-                  backgroundImage: `url(${slideImage.url})`,
-                }}
-              >
-                <span style={spanStyle}>{slideImage.caption}</span>
-              </div>
-            </div>
-          ))}
-        </Slide>
-      </div>
-      <p>{project.conceptWriteup}</p>
-      <h2>Concept Sketches</h2>
-      <div className="slide-container">
-        <Slide {...properties}>
-          {project.detailedImages.map((slideImage, index) => (
-            <div key={index}>
-              <div
-                style={{
-                  ...divStyle,
-                  backgroundImage: `url(${slideImage.url})`,
-                }}
-              >
-                <span style={spanStyle}>{slideImage.caption}</span>
-              </div>
-            </div>
-          ))}
-        </Slide>
-      </div>
-      <p>{project.detailedWriteup}</p>
-      <h2>Video</h2>
-      <iframe
-        width="560"
-        height="315"
-        src="https://www.youtube.com/embed/RtVM1qv3i3Y?si=p7L_hvGaUazbKtmp"
-        title="YouTube video player"
-        frameBorder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        allowfullscreen
-      ></iframe>
     </div>
   );
 }
