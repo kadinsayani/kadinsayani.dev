d620bc1b866f442d5417791b5d238da1bdbddc58
Kadin Sayani <me@kadinsayani.dev>
2023-10-01 17:45:17 -0600

    updated to use custom slide component

--- src/routes/project.jsx
+++ src/routes/project.jsx
@@ -5,28 +5,8 @@ import { PropTypes } from "prop-types";
 import { IoArrowBack } from "react-icons/io5";
 import { IoLogoGithub } from "react-icons/io5";
 import { IoLink } from "react-icons/io5";
-import { Slide } from "react-slideshow-image";
-import "react-slideshow-image/dist/styles.css";
+import Slide from "../components/slide";
 import ScrollToTop from "../components/scrollToTop.jsx";
-
-const spanStyle = {
-  padding: "10px",
-  background: "#efefef",
-  color: "#000000",
-  position: "absolute",
-  bottom: "0",
-};
-
-const divStyle = {
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
-};
-
-const properties = {
-  arrows: false,
-  indicators: true,
-};
 
 function Project(props) {
   const navigate = useNavigate();
@@ -56,22 +36,7 @@ function Project(props) {
         <p>{project.description}</p>
         <div className="skip">
           <h2>Detailed Sketches</h2>
-          <div className="slide-container">
-            <Slide {...properties}>
-              {project.detailedImages.map((slideImage, index) => (
-                <div style={divStyle} key={index}>
-                  <img
-                    key={index}
-                    src={slideImage.url}
-                    width={800}
-                    height={600}
-                    alt={`Detailed Sketch ${index + 1}`}
-                  />
-                  <span style={spanStyle}>{slideImage.caption}</span>
-                </div>
-              ))}
-            </Slide>
-          </div>
+          <Slide images={project.detailedImages} />
           <div className="writeup" style={{ textAlign: "left" }}>
             <ol>
               {project.detailedPoints.map((detailedPoint, index) => (
@@ -83,22 +48,7 @@ function Project(props) {
             ))}
           </div>
           <h2>Concept Sketches</h2>
-          <div className="slide-container">
-            <Slide {...properties}>
-              {project.conceptImages.map((slideImage, index) => (
-                <div style={divStyle} key={index}>
-                  <img
-                    key={index}
-                    src={slideImage.url}
-                    width={800}
-                    height={600}
-                    alt={`Detailed Sketch ${index + 1}`}
-                  />
-                  <span style={spanStyle}>{slideImage.caption}</span>
-                </div>
-              ))}
-            </Slide>
-          </div>
+          <Slide images={project.conceptImages} />
           <div className="writeup" style={{ textAlign: "left" }}>
             <ol>
               {project.conceptPoints.map((conceptPoint, index) => (
